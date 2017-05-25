e6d0713396c73d4dd8e33cd6ab225101a3f85266
Lucas PAGE-CACCIA <lucas.page-caccia@mail.mcgill.ca>
2017-05-25 11:11:38 -0400

    fixed update bug

--- train_began.py
+++ train_began.py
@@ -9,7 +9,7 @@ params['batch_size'] = 64
 params['tensor_shape'] = (64, 3, 64, 64)
 params['initial_eta'] = 1e-4
 params['lambda_k'] = 0.001
-params['gamma'] = 0.5
+params['gamma'] = 0.7
 params['load_weights'] = None #(12, 1000)#None# version/epoch tupple pair
 params['optimizer'] = 'adam'
 params['image_prepro'] = 'DCGAN' # (/250.; -0.5; /0.5) taken from DCGAN repo.
@@ -21,8 +21,8 @@ generator_layers = generator()
 encoder_layers = encoder()
 decoder_layers = generator()
 generator = generator_layers[-1]
-encoder = encoder_layers[-1]
-decoder = decoder_layers[-1]
+encoder = encoder_layers[-1] # discriminator
+decoder = decoder_layers[-1] # discriminator
 
 dh = DataHandler(tensor_shape=params['tensor_shape'])
 eh = ExpHandler(params, test=params['test'])
@@ -38,7 +38,8 @@ lambda_k = params['lambda_k']
 gamma = params['gamma']
 
 # params
-all_params = ll.get_all_params([encoder, decoder, generator], trainable=True)
+gen_params = ll.get_all_params(generator, trainable=True)
+disc_params = ll.get_all_params([encoder, decoder], trainable=True)
 
 # real "loss" 
 real_enc_imgs = ll.get_output(encoder, inputs=images) # encoding of real images
@@ -55,11 +56,16 @@ loss_fake = lasagne.objectives.squared_error(fake_rec_imgs, fake_imgs).mean()
 gen_loss = loss_fake
 disc_loss = loss_real - k * loss_fake
 total_loss = gen_loss + disc_loss
-all_grads = theano.grad(total_loss, wrt=all_params)
-grads_norm = sum(T.sum(T.square(grad)) for grad in all_grads) / len(all_grads)
+gen_grads = theano.grad(gen_loss, wrt=gen_params)
+disc_grads = theano.grad(disc_loss, wrt=disc_params)
+gen_grads_norm = sum(T.sum(T.square(grad)) for grad in gen_grads) / len(gen_grads)
+disc_grads_norm = sum(T.sum(T.square(grad)) for grad in disc_grads) / len(disc_grads)
 
 # updates
-updates = optimizer_factory(params['optimizer'], all_grads, all_params, eta)
+updates_gen = optimizer_factory(params['optimizer'], gen_grads, gen_params, eta)
+updates_disc = optimizer_factory(params['optimizer'], disc_grads, disc_params, eta)
+all_updates = updates_gen.copy()
+all_updates.update(updates_disc)
 
 # setting BEGAN parameters
 M_global = loss_real + T.abs_(gamma*loss_real - loss_fake)
@@ -69,7 +75,8 @@ fn_output = OrderedDict()
 fn_output['loss_real_mean'] = loss_real
 fn_output['loss_fake_mean'] = loss_fake
 fn_output['k'] = k
-fn_output['grads_norm'] = grads_norm
+fn_output['gen_grads_norm'] = gen_grads_norm
+fn_output['disc_grads_norm'] = disc_grads_norm
 fn_output['M_global'] = M_global
 
 eh.add_model('gen', generator_layers, fn_output)
@@ -80,7 +87,7 @@ eh.add_model('enc', encoder_layers, fn_output)
 print 'compiling functions'
 train_model = theano.function(inputs=[index, k], 
                               outputs=fn_output.values(), 
-                              updates=updates,
+                              updates=all_updates, 
                               givens={images: dh.GPU_image[index * params['batch_size']: 
                                                        (index+1) * params['batch_size']]},
                               name='train_model')
@@ -99,7 +106,7 @@ for epoch in range(3000000):
     for _ in range(params['epoch_iter']):
         batch_no = dh.get_next_batch_no()
         model_out = train_model(batch_no, k_value)
-        loss_real_mean, loss_fake_mean, k_value, _, _ = model_out
+        loss_real_mean, loss_fake_mean, k_value, _, _, _ = model_out
 
         # perform [hyper]parameter updates
         k_new = k_value + lambda_k * (gamma*loss_real_mean - loss_fake_mean)
